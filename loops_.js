8e0933c98f7d01ac392cd99d64c55025af0cfe5b
Govinda <govinda@gmail.com>
2024-05-02 14:00:44 +0530

    changes

---
+++ loops_.js
@@ -0,0 +1,30 @@
+//Q1 find the even number between 0 to 100 .
+// solution by for loop
+// for ( count=0; count<=100 ; count++) {
+//     if(count%2===0) {
+//         console.log(count);
+//     }
+// }
+
+
+
+//solution by while loop
+// let num = 0 ;
+// while(num<=100){
+//     if (num%2===0){
+//         console.log(num);
+//     }num++;
+// }
+
+
+
+
+//Solution by do-while loop
+// let number = 0 ;
+// do {
+//     if(number % 2 === 0){
+//         console.log(number);
+//     }
+//     number++;
+// }
+// while(number <= 100);
