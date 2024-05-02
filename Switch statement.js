94cf3589519b2775fc4d9e5f8b44c92289f151c1
Govinda <govinda@gmail.com>
2024-05-02 11:43:15 +0530

    first commit

---
+++ Switch statement.js
@@ -0,0 +1,36 @@
+let day = prompt("Enter your favorite day  ")
+// let day = "Monday"
+switch (day) {
+     case "Sunday" :
+        alert("“Sundays should be about you and things that make you happy”");
+        console.log("“Sundays should be about you and things that make you happy”")
+        break;
+      case "Monday":
+        alert(" “Monday: perfect time to reboot and start fresh. Good morning let’s start Monday morning out with a smile.”");
+        console.log("“Monday: perfect time to reboot and start fresh. Good morning let’s start Monday morning out with a smile.")
+        break;
+      case "Tuesday":
+        alert("“Tuesday is the day of the week when you have come out of Monday blues and your routine starts running smoothly.”");
+        console.log("“Tuesday is the day of the week when you have come out of Monday blues and your routine starts running smoothly.”")
+        break;
+      case "Wednesday":
+        alert(" “Wednesday will either make you or break you. You are either on the upward trend or the downward slide to the end of the work week. Give it all you got this Wednesday!”");
+        console.log("“Wednesday will either make you or break you. You are either on the upward trend or the downward slide to the end of the work week. Give it all you got this Wednesday!” ")
+        break;
+      case "Thursday":
+        alert(" “Thursday is full of tenderness and kindness. So have a good day, and share it with those around you.” ");
+        console.log("“Thursday is full of tenderness and kindness. So have a good day, and share it with those around you.”")
+        break;
+      case "Friday":
+        alert(" “Friday is finally here again. The end of the work week. And the start of the weekend.”");
+        console.log("“Friday is finally here again. The end of the work week. And the start of the weekend.”")
+        break;
+      case "Saturday":
+        alert("“Saturday night is when you want to do what you want to do and not what you have to do.”");
+        console.log("“Saturday night is when you want to do what you want to do and not what you have to do.”")
+        break;
+      default :
+        alert ("Invaid input. ! Have a good day")
+        console.log ("Invaid input. ! Have a good day")
+    };
+    
