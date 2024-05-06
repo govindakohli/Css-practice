9a01d5d451d026500c28a14991154124174b36b3
Govinda <govinda@gmail.com>
2024-05-06 17:59:39 +0530

    change

---
+++ function_.js
@@ -0,0 +1,24 @@
+// How to create a function in javascript 
+// function myFunction() {
+//     console.log("Hello ! i am function");
+//     console.log("Hello ! i am function");
+//     console.log("Hello ! i am function");
+//     console.log("Hello ! i am function");
+// }
+// How to call a function
+// myFunction();
+// Hello ! i am function
+// Hello ! i am function
+// Hello ! i am function
+// Hello ! i am function
+
+// Arrow Function 
+// const arrowMul = (a = prompt("enter Your age ") ) => {
+//    if ( a >=18) {
+//     console.log("Congratulation!. You are eligible for the driving.")
+//    }
+//   else (
+//     console.log("Sorry! You are not eligible for the driving.")
+//   )
+
+// }
