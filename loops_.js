079680f5be0654796c0a271f810dae8b048b593b
Govinda <govinda@gmail.com>
2024-05-09 11:08:35 +0530

    change

--- loops_.js
+++ loops_.js
@@ -28,3 +28,19 @@
 //     number++;
 // }
 // while(number <= 100);
+
+
+//  for ( let i = 2 ; i<=20 ; i=i+2) {
+//     console.log(i)
+//  }
+// odd number from  4 to 23 
+// methode 1 
+// for ( let i = 4 ; i<=23 ; i++) {
+//     if (i % 2 !== 0) {
+//         console.log(i); 
+//     }
+//  }
+// print  1 , 5 , 9 , 13 , 17
+// for ( let i = 1 ; i<=17 ; i=i+4) {
+//         console.log(i)
+// }
