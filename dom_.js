6b2b334165931ddef9247061eb045cadf41675f2
Govinda <govinda@gmail.com>
2024-05-11 18:30:59 +0530

    change

---
+++ dom_.js
@@ -0,0 +1,29 @@
+// DOM methods in JavaScript
+
+//1.  Element Selection by id 
+let ID = document.getElementById("btn");
+// console.dir(ID);
+  // console.log(ID)
+
+//2.  Element Selection by Class 
+ let Class = document.getElementsByClassName("heading");
+//  console.dir(Class);
+//  console.log(Class);
+
+//3. Elemet Selection by tag name 
+let TagName = document.getElementsByTagName( "h1");
+// console.dir(TagName);
+
+    // By Query selector 
+//4. Element Selection by QuerySelector - three of kind of request like 1. by #id , .class , tagname ..
+// let QuerySelector = document.querySelector("#btn");
+// console.dir ( QuerySelector);
+
+//5. Element Selection by QuerySelectorAll
+let QuerySelectorAll = document.querySelectorAll("he");
+// console.dir(QuerySelectorAll);
+
+let getatt = element.getAttribute("heading");
+console.dir (getatt)
+
+
